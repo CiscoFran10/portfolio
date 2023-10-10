98bb60edb3fe608a86d9c354dcf1371d687817d1
Thiago Rodrigues <thiago.rodrigues@ibm.com>
2023-10-09 22:52:08 -0300

    fix: images path

--- src/data.js
+++ src/data.js
@@ -2,63 +2,63 @@ export const data = [
   {
     link: "https://ibkl.com.br/",
     title: "IBKL +",
-    image: "/../images/ibkl.png",
+    image: "/images/ibkl.png",
   },
   {
     link: "https://ciscofran10.github.io/Telier/index.html",
     title: "Telier",
-    image: "/../images/telier.png",
+    image: "/images/telier.png",
   },
   {
     link: "https://bbsc-digital.vercel.app/",
     title: "Ann",
-    image: "/../images/ann.png",
+    image: "/images/ann.png",
   },
   {
     link: "https://ciscofran10.github.io/Florencia/",
     title: "Florencia",
-    image: "/../images/florencia.png",
+    image: "/images/florencia.png",
   },
   {
     link: "https://energy-phi.vercel.app/",
     title: "Enermatch",
-    image: "/../images/enermatch.png",
+    image: "/images/enermatch.png",
   },
   {
     link: "https://volvo-nu.vercel.app/",
     title: "Volvo",
-    image: "/../images/volvo.png",
+    image: "/images/volvo.png",
   },
   {
     link: "https://scroll-trigger-gsap.vercel.app/",
     title: "Charlotte LaRue",
-    image: "/../images/charlotte.png",
+    image: "/images/charlotte.png",
   },
   {
     link: "https://ciscofran10.github.io/Zoogram/",
     title: "Zoogram",
-    image: "/../images/zoogram.png",
+    image: "/images/zoogram.png",
   },
   {
     link: "https://mtd-eosin.vercel.app/",
     title: "Mtd",
-    image: "/../images/mtd.png",
+    image: "/images/mtd.png",
   },
 
   {
     link: "https://xcommerce-eight.vercel.app/",
     title: "Xcommerce",
-    image: "/../images/xcommerce.png",
+    image: "/images/xcommerce.png",
   },
   {
     link: "https://react-dev-hub.vercel.app/",
     title: "Dev Hub",
-    image: "/../images/devhub.png",
+    image: "/images/devhub.png",
   },
 
   {
     link: "https://react-nu-finance.vercel.app/",
     title: "Nu Finance",
-    image: "/../images/nu-finance.png",
+    image: "/images/nu-finance.png",
   },
 ];
